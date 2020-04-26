d41c17b8c0b5c40bb7a27369bcc40aabbb59e4ee
Jean-Luc Moulard <ojo.djiele@gmail.com>
2020-04-26 23:29:57 +0200

    Add infos about classes like extends, implements, isAbstract and isFinal

--- src/Djiele/PHP/Autoloader.php
+++ src/Djiele/PHP/Autoloader.php
@@ -22,17 +22,19 @@ class Autoloader
                 $this->classMap = include $classMapFile;
             } else {
                 self::setClassMap();
-                if(is_writable($this->classMapDir)) {
-                   file_put_contents($classMapFile, '<?php return ' . var_export($this->classMap, true) . ';', LOCK_EX);
-                } else {
-                    trigger_error('Can not write to ' . $classMapDir, E_USER_NOTICE);
-                }
+				if(false === $this->noCache) {
+					if(is_writable($this->classMapDir)) {
+					   file_put_contents($classMapFile, '<?php return ' . var_export($this->classMap, true) . ';', LOCK_EX);
+					} else {
+						trigger_error('Can not write to ' . $classMapDir, E_USER_NOTICE);
+					}
+				}
             }
         }
 
         spl_autoload_register(function($class) {
             if(array_key_exists($class, $this->classMap)) {
-                $fileCandidate = $this->classMap[$class];
+                $fileCandidate = $this->classMap[$class]['in_file'];
                 if(is_file($fileCandidate) && is_readable($fileCandidate)) {
                     include $fileCandidate;
                     return true;
@@ -61,11 +63,35 @@ class Autoloader
         
         return $this;
     }
+	
+	public function getClassInfos($class) {
+		if (array_key_exists($class, $this->classMap)) {
+			
+			return $this->classMap[$class];
+		}
+		
+		return null;
+	}
+	
+	public function getImplementers($interface) {
+		$ret = [];
+		if(!is_array($interface)) {
+			$interface = func_get_args();
+		}
+		foreach ($this->classMap as $class => $data) {
+			if ($interface == array_intersect($interface, $data['implements'])) {
+				$ret[] = $class;
+			}
+		}
+		
+		return $ret;
+	}
     
     protected function setClassMap() 
     {
         $this->classMap = [];
         $ltrimLen = strlen(__DIR__)+1;
+		$lastClassFound = null;
         
         while (null !== ($folder = array_shift($this->folders))) {
             
@@ -81,12 +107,67 @@ class Autoloader
                    $currentNamespace = '';
                    $namespaceFound = false;
                    $classFound = false;
+				   $classType = 'unknown';
                    foreach ($tokens as $i => $token) {
                         if (is_array($token) && T_NAMESPACE == $token[0]) {
                             $namespaceFound = true;
                             $namespaceAccu = '';
                         } elseif (is_array($token) && (T_CLASS == $token[0] || T_INTERFACE == $token[0] || T_TRAIT == $token[0])) {
                             $classFound = true;
+							if (T_CLASS == $token[0]) {
+								$classType = 'class';
+							} elseif(T_INTERFACE == $token[0]) {
+								$classType = 'interface';
+							} elseif(T_TRAIT == $token[0]) {
+								$classType = 'trait';
+							} else {
+								$classType = 'unknown';
+							}
+						} elseif (is_array($token) && T_EXTENDS == $token[0]) {
+							$j = ($i +1);
+							$extendAccu = '';
+							do {
+								$j++;
+								if(is_array($tokens[$j])) {
+									if(T_NS_SEPARATOR == $tokens[$j][0]) {
+										$extendAccu .= '\\';
+									} else {
+										$extendAccu .= $tokens[$j][1];
+									}
+								}
+							} while(T_WHITESPACE !== $tokens[$j][0]);
+							if (false === strpos($extendAccu, '\\')) {
+								$extendAccu = $currentNamespace . '\\' . $extendAccu;
+							}
+							$this->classMap[$lastClassFound]['extends'] = $extendAccu;
+						} elseif (is_array($token) && T_IMPLEMENTS == $token[0]) {
+							$j = $i;
+							$interfaceAccu = '';
+							do {
+								$j++;
+								if(is_array($tokens[$j])) {
+									if(T_WHITESPACE == $tokens[$j][0]) {
+										continue;
+									} elseif(T_NS_SEPARATOR == $tokens[$j][0]) {
+										$interfaceAccu .= '\\';
+									} else {
+										$interfaceAccu .= $tokens[$j][1];
+									}
+								} elseif(',' == $tokens[$j]) {
+									if (false === strpos($interfaceAccu, '\\')) {
+										$interfaceAccu = $currentNamespace . '\\' . $interfaceAccu;
+									}
+									$this->classMap[$lastClassFound]['implements'][] = $interfaceAccu;
+									$interfaceAccu = '';
+									continue;
+								}
+							} while('{' !== $tokens[$j+1]);
+							if('' != $interfaceAccu) {
+								if (false === strpos($interfaceAccu, '\\')) {
+									$interfaceAccu = $currentNamespace . '\\' . $interfaceAccu;
+								}
+								$this->classMap[$lastClassFound]['implements'][] = $interfaceAccu;
+							}
                         } elseif (is_array($token) && T_WHITESPACE == $token[0]) {
                             continue;
                         } else if($namespaceFound && is_array($token) && T_STRING == $token[0]) {
@@ -96,8 +177,26 @@ class Autoloader
                             $currentNamespace = $namespaceAccu;
                             $namespaceFound = false;
                         } elseif($classFound) {
-                            $this->classMap[ltrim($currentNamespace . '\\' . $token[1], '\\')] = $candidate; //substr($candidate, $ltrimLen);
+							$isAbstract = false;
+							$isFinal = false;
+							if(isset($tokens[$i-4]) && is_array($tokens[$i-4])) {
+								if(T_ABSTRACT == $tokens[$i-4][0]) {
+									$isAbstract = true;
+								} elseif(T_FINAL == $tokens[$i-4][0]) {
+									$isFinal = true;
+								}
+							}
+							$lastClassFound = ltrim($currentNamespace . '\\' . $token[1], '\\');
+                            $this->classMap[$lastClassFound] = [
+								'type' => $classType,
+								'is_final' => $isFinal,
+								'is_abstract' => $isAbstract,
+								'extends' => null,
+								'implements' => [],
+								'in_file' => $candidate
+							];
                             $classFound = false;
+							$classType = 'unknown';
                         }
                    }
                }
